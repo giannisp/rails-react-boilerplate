91b16042d6beca2caa5a33b1902919874d3810ed
Ioannis Poulakas <giannis.p@gmail.com>
2017-01-31 00:06:14 +0200

    Set home/index as root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'home/index'
 
+  root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
