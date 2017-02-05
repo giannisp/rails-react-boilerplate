f236eb18cb77d7696f35a5a005a3ffd877e853ba
Ioannis Poulakas <giannis.p@gmail.com>
2017-02-05 15:04:55 +0200

    Add a route for returning current timestamp

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def timestamp
+    render json: {timestamp: Time.now.to_i}
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'home/index'
+  get 'home/timestamp'
 
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
