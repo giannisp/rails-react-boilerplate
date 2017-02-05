f278652a4cf31153d1509ed105cd9f3be0684cf3
Ioannis Poulakas <giannis.p@gmail.com>
2017-02-05 12:27:50 +0200

    Add manifest path and root assets path for asset loader

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+Rails.application.config.assets.root_path = '/dist/'
+Rails.application.config.assets.manifest = 'public/dist/manifest.json'
